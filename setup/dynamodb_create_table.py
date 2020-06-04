2148c0cbb81484b63802fe1ec531d8e5bfb39a02
Cate Devey <cdevey@nd.edu>
2020-06-04 19:49:08 -0400

    add function to read from csv and write sample data to dyndb table

--- setup/dynamodb_create_table.py
+++ setup/dynamodb_create_table.py
@@ -1,7 +1,7 @@
 import boto3
 
 
-def create_table(dynamodb=None):
+def create_table(dynamodb=None, table_name):
     dynamodb = boto3.client('dynamodb')
 
     response = client.create_table(
@@ -15,7 +15,7 @@ def create_table(dynamodb=None):
               'AttributeType': 'S'
           }
       ],
-      TableName='multiplayer_input',
+      TableName= table_name,
       KeySchema=[
           {
               'AttributeName': 'week',
@@ -28,9 +28,27 @@ def create_table(dynamodb=None):
       ]
   )
 
-    return table
+    return response
 
+def add_sample_data(data, table_name):
+    dynamodb = boto3.resource('dynamodb')
+    db = dynamodb.Table(table_name)
+
+    items = []
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data = {}
+            data['temp'] = row['temp']
+            #populate remaining fields here
+            items.append(data)
+
+    for item in items:
+        response = db.put_item(Item = item)
 
 if __name__ == '__main__':
-    db_table = create_table()
+    table_name = 'multiplayer_input'
+
+
+    db_table = create_table(table_name)
     print("Table status:", db_table.table_status)
