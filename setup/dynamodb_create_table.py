bf21b38cd7c731bedb3b4b4857c94ea95ea20089
Cate Devey <cdevey@nd.edu>
2020-06-05 17:55:21 -0400

    add sample data file and adjust function to add multiplayer sample data to convert csv to json

--- setup/dynamodb_create_table.py
+++ setup/dynamodb_create_table.py
@@ -31,25 +31,37 @@ def create_multiplayer_table(table_name, dynamodb=None):
 
     return response
 
-def add_sample_data(data, table_name):
+def add_sample_data(table_name):
     dynamodb = boto3.resource('dynamodb')
     db = dynamodb.Table(table_name)
+    # file = multiplayer_table_sample_data.csv
 
     items = []
-    with open(file) as csvfile:
+    with open('multiplayer_table_sample_data.csv') as csvfile:
         reader = csv.DictReader(csvfile)
+
         for row in reader:
+            #create dict to store csv data
             data = {}
-            data['temp'] = row['temp']
-            #populate remaining fields here
+            #map csv fields to dict fields
+            data['week'] = row['Week']
+            data['team'] = row['Team']
+            data['multiplayer'] = row['Multiplayer']
+            data['seed'] = row['Seed']
             items.append(data)
+    # print(items)
 
     for item in items:
         response = db.put_item(Item = item)
 
+    return response
+
 if __name__ == '__main__':
     table_name = 'multiplayer_input'
 
+    #create multiplayer_table ONLY RUN THIS FUNCTION ONCE, CHECK CONSOLE TO SEE IF TABLE ALREADY EXISTS
+    # db_table = create_multiplayer_table(table_name)
+    # print("Table status:", db_table.table_status)
 
-    db_table = create_multiplayer_table(table_name)
-    print("Table status:", db_table.table_status)
+    #add sample data to existing table
+    data_add_status = add_sample_data(table_name)
